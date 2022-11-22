98ec99d6c22b3203ae6c7f35e23c0f3968ba9073
xltan98 <116934303+xltan98@users.noreply.github.com>
2022-11-22 21:18:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,23 +19,42 @@ import joblib
 app = Flask(__name__)
 
 
-# In[ ]:
+# In[4]:
 
 
-from flask import request,render_template
+from flask import request, render_template
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        area = float(request.form.get("area"))
-        print(area)
-        year =float(request.form.get("year"))
-        print(year)
-
-        model = joblib.load("randomforest.joblib")
-        r1 = model.predict([[price]])
-       
-        return(render_template("index.html",result=r1))
+        Year = request.form.get("Year")
+        floor_area_sqm = request.form.get("floor_area_sqm")
+        lease_commence_date = request.form.get("lease_commence_date")
+        Remaining_lease_updated = request.form.get("Remaining_lease_updated")
+        
+        model1 = joblib.load("randomforest.joblib")
+        pred = model1.predict([[Year,floor_area_sqm,lease_commence_date,Remaining_lease_updated]])
+        s = f""" Your requested property is: 
+            Year: {Year}
+            Floor Area: {floor_area_sqm}
+            Lease Commense Date: {lease_commence_date} 
+            Remaining Lease: {Remaining_lease_updated}
+            The property is valued at {str(pred[0])} """
+        
+        return(render_template("index.html",results="s"))
     else:
-        return(render_template("index.html",result ="waiting"))
+        return(render_template("index.html",results="Loading..."))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
 
